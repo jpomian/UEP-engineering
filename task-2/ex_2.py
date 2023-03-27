4e61fadf92cdd893c161518bca0ccde3c9fcc0b2
Jędrzej Pomianowski <69614@student.ue.poznan.pl>
2023-03-27 12:12:02 +0200

    Added rectangle.

--- task-2/ex_2.py
+++ task-2/ex_2.py
@@ -1,7 +1,19 @@
 # kwadrat
 
+from asyncio.proactor_events import _ProactorSocketTransport
+
+
 a=10
 size=a*4
 surface=a**2
+
+print('Obwód: ' + str(size) + '\nPole: ' + str(surface))
+
+# prostokat
+
+a=5
+b=8
+size=(a*2)+(b*2)
+surface=a*b
 
 print('Obwód: ' + str(size) + '\nPole: ' + str(surface))
